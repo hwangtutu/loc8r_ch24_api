055e1a324be6f222b8d2de455127bd8ac154b6aa
hwangtutu <163239019+hwangtutu@users.noreply.github.com>
2025-12-05 21:00:04 +0900

    Update app.js

--- app.js
+++ app.js
@@ -8,42 +8,38 @@ const passport = require('passport');
 require('./app_api/models/db');
 require('./app_api/config/passport');
 
-//var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/users');
-//var indexRouter = require('./app_server/routes/index');
-const indexRouter = require('./app_server/routes/index');
 const apiRouter = require('./app_api/routes/index');
 var usersRouter = require('./app_server/routes/users');
 
 var app = express();
 
+// CORS
 const cors = require('cors');
-const corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200 // For legacy browser support
-};
-app.use(cors(corsOptions));
+const allowedOrigins = [
+  'http://localhost:4200',
+  'https://loc8rauth24-2021810076.netlify.app'
+];
 
-app.use('/api', (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-with, Content-type, Accept, Authorization");
-    next();
-});
+app.use('/api', cors({
+  origin: (origin, cb) => {
+    // Postman, curl 등 origin 없는 요청 허용
+    if (!origin) return cb(null, true);
+    return cb(null, allowedOrigins.includes(origin));
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'Authorization'
+  ],
+  optionsSuccessStatus: 200
+}));
 
 // view engine setup
-//app.set('views', path.join(__dirname, 'views'));
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
-
-app.use('/api', (req, res, next) => {
-    // res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    next();
-});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -53,18 +49,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app_public', 'build')));
 app.use(passport.initialize());
 app.use(express.static(path.join(__dirname, 'app_public')));
-app.use('/', indexRouter);
+
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 
 // error handlers
-// Catch unauthorised errors
 app.use((err, req, res, next) => {
-    if (err.name === 'UnauthorizedError') {
-        res
-            .status(401)
-            .json({ "message": err.name + ": " + err.message });
-    }
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({ message: err.name + ': ' + err.message });
+  }
 });
 
 // catch 404 and forward to error handler
@@ -74,11 +67,8 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
